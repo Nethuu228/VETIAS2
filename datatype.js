7416645d13d8c73dd550c8dc05dea45f1b3a2b96
Nethuu228 <nethrasree2287@gmail.com>
2026-06-12 14:59:00 +0530

    Added datatype.js

---
+++ datatype.js
@@ -0,0 +1,12 @@
+//Object
+var age =20
+console.log(age)
+//string
+var name="ishu"
+console.log(name)
+//boolean 
+var student =true
+console.log(student)
+var emptyvalue =null
+console.log(emptyvalue)
+console.log("or object")
