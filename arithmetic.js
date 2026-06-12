867e95b6ad2252191b9d6d004cb0ae88a00060fc
Nethuu228 <nethrasree2287@gmail.com>
2026-06-12 11:41:16 +0530

    Added arithmetic.js

--- arithmetic.js
+++ arithmetic.js
@@ -7,3 +7,20 @@ console.log(a*b)//multiplication
 console.log(a/b)//division
 console.log(a%b)//module
 console.log(a**b)//exponentation
+console.log("Assigment Operator")
+var c=25;
+console.log(a+=5)
+console.log(a-=5)
+console.log(a*=5)
+console.log(a/=5)
+console.log(a%=5)
+console.log(a**=5)
+console.log("Comparison operator")
+var a=5;
+var b=10;
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
