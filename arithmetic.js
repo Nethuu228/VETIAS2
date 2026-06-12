b7fb743b70e012f1e0f22f092a367a47c2602b39
Nethuu228 <nethrasree2287@gmail.com>
2026-06-12 11:14:59 +0530

    Added arithmetic.js

---
+++ arithmetic.js
@@ -0,0 +1,9 @@
+//arithmetic
+var a=15;
+var b=25;
+console.log(a+b)//addition
+console.log(a-b)//subtraction
+console.log(a*b)//multiplication
+console.log(a/b)//division
+console.log(a%b)//module
+console.log(a**b)//exponentation
