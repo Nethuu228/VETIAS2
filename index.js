4122a3483226486f28b00e2c1cb16cf65e43462c
Nethuu228 <nethrasree2287@gmail.com>
2026-06-12 10:26:02 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//variable declaration using var
+var name ="Vellalar"
+console.log(name)
+//variable declaration using let
+{
+    let age =25
+    console.log(age)
+}
+//variable declaration using const
+const country="India"
+console.log(country)
