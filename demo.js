f4c0066c851aeff7eb4fc47feb286d45fba6fd93
Nethuu228 <nethrasree2287@gmail.com>
2026-06-15 11:22:16 +0530

    (Added demo.js)

---
+++ demo.js
@@ -0,0 +1,32 @@
+// FOR LOOP
+console.log("FOR LOOP");
+for(let i = 1; i <= 10; i++) {
+    console.log("2 * " + i + " = " + (2 * i));
+}
+
+// WHILE LOOP
+console.log("WHILE LOOP");
+let j = 1;
+while(j <= 10) {
+    console.log("2 * " + j + " = " + (2 * j));
+    j++;
+}
+
+// DO WHILE LOOP
+console.log("DO WHILE LOOP");
+let k = 1;
+do {
+    console.log("2 * " + k + " = " + (2 * k));
+    k++;
+} while(k <= 10);
+
+//DO WHILE LOOP
+
+let count = 6
+do
+{
+    console.log(count);
+    count++;
+}
+while(count <= 5)
+    console.log("outside do...while")
